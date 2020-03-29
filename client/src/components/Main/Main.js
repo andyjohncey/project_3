38b061f588c59ab49fbc25cabcaaa751054d07e2
andrew.johncey@outlook.com <andrew.johncey@outlook.com>
2020-03-29 22:35:37 +1100

    add Main component

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import channels from '../Channels/Channels';
+import Post from '../Post/Post';
+
+
+function Main() {
+    return (
+        <>
+
+            <h1>Emergency call Dial '000'</h1>
+            {/* <Channels /> */}
+            <Post />
+        </>
+
+    )
+}
+
+export default Main;
