f30d939e326d7e8bacf68729397f3235ee991032
Andy J <andrew.johncey@outlook.com>
2020-04-01 13:55:36 +1100

    add authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import AuthContext from './context/auth-context';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
-import { StoreProvider } from './utils/GlobalState';
 import Nav from './components/Nav/Nav';
 import Assist from './pages/Assist';
+import Home from './pages/Home';
+
+export default function App(){
+
+  const [state, setState] = useState({
+    token: null,
+    userId: null,
+  })
 
 
-function App() {
+  const login = (token, userId, tokenExpiration) => {
+    setState({token: token, userId: userId})
+  }
+  const logout = (token, userId, tokenExpiration) => {
+    setState({token: null, userId: null})
+  }
+
   return (
     <Router>
-      <div>
-        <StoreProvider>
-          <Nav />
+       <AuthContext.Provider value={{
+        token: state.token,
+        userId: state.userId,
+        login: login, 
+        logout: logout,
+      }}>
+      <Nav />
+        <div className={classes.root}>
           <Switch>
-            <Route path="/" component={Assist} />
-            <Route path="/posts" component={Assist} />
-            <Route path="/post/:id" component={Detail} />
-            <Route component={NoMatch} />
+            {state.token && <Route path="/" component={Home} />}
+            {state.token && <Route path="/posts" component={Home} />}
+            {state.token && <Route path="/post/:id" component={Detail} />}
+            {!state.token && <Route path="/auth" component={AuthPage}/>}
+            {!state.token && <Redirect from="/" to="/auth" exact/>}
+            {!state.token && <Redirect to="/auth" exact/>}
+            {!state.token && <Route component={NoMatch} />}
           </Switch>
-        </StoreProvider>
-      </div >
+        </div>
+      </AuthContext.Provider>
     </Router>
   );
-}
 
-export default App;
+
+}
