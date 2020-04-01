fa3f826162667e52f8d458f255df58398f7ac034
Andy J <andrew.johncey@outlook.com>
2020-04-01 13:55:01 +1100

    add authentication

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink} from 'react-router-dom';
+import AuthContext from '../context/auth-context';
 // import ReactDOM from 'react-dom';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
@@ -41,7 +43,8 @@ const theme = createMuiTheme({
 });
 
 const Nav = (props) => {
-    const [state, dispatch] = useStoreContext();
+    // const [state, dispatch] = useStoreContext();
+    const {token, logout} = useContext(AuthContext);
 
     const handleIconClicked = () => {
         dispatch({
@@ -71,8 +74,16 @@ const Nav = (props) => {
                     Aged Care Worker Assist
                 </Typography>
                 <div style={{ marginLeft: "auto" }}>
-                    <Button color="inherit" href="./Login/Login">
-                        login
+                {!token && <li><NavLink to="/auth">Authentication</NavLink></li>}
+                {token && (
+                    <li><NavLink to="/logout" onClick={logout}>Logout</NavLink></li>
+                )}
+                    <Button color="inherit">
+                    {!token && <NavLink to="/auth">Authentication</NavLink>}
+                    </Button>
+                    <br></br>
+                    <Button color="inherit">
+                    {token && ( <NavLink to="/logout" onClick={logout}>Logout</NavLink>)}
                     </Button>
                 </div>
             </Toolbar>
@@ -80,6 +91,32 @@ const Nav = (props) => {
     );
 }
 
-
-
 export default Nav;
+
+// import React, {useContext} from 'react';
+// import { NavLink } from 'react-router-dom';
+// import AuthContext from '../context/auth-context';
+// import './MainNavigation.css'
+
+// export default (props) => {
+
+//     const {token, logout} = useContext(AuthContext);
+    
+//     return (
+//         <header className="main-nav">
+//         <div className="main-nav-logo">
+//             <h1>The Navbar</h1>
+//         </div>
+//         <nav className="main-nav-items">
+//             <ul>
+//                 {!token && <li><NavLink to="/auth">Authentication</NavLink></li>}
+//                 {token && (
+//                     <li><NavLink to="/logout" onClick={logout}>Logout</NavLink></li>
+//                 )}
+//             </ul>
+//         </nav>
+//         </header>
+//     )
+    
+
+// }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AuthContext from '../context/auth-context';
 // import { Col, Row, Container } from '';
 import Post from '../components/Post/Post';
 import Nav from '../components/Nav/Nav';
@@ -6,15 +7,34 @@ import ChannelsList from '../components/ChannelsList.js';
 import Sidebar from '../components/SideBar/SideBar';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import ListItem from '@material-ui/core';
+import Typography from '@material-ui/core';
 // import Assist from '../pages/Assist';
 import SideBar from "../components/SideBar/SideBar";
 import Nav from '../components/Nav';
 import Post from '../components/Post';
 import ChannelsList from '../components/ChannelsList';
 
+export default function App(){
+
+    const {token} = useContext(AuthContext);
+  
+    const [friends, setFriends] = useState([])
+    useEffect(async ()=>{
+      const result = await fetch('/api/friends', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+      });
+      if(result.status !== 200 && result.status !== 201){
+        throw new Error('Failed!')
+      }
+      const resultJSON = await result.json();
+      setFriends(resultJSON.friends)
+    }, [])
 
 
-function Home() {
 
 
     return (
@@ -23,13 +43,19 @@ function Home() {
                 <Nav />
                 <SideBar />
                 {/* <h1>Channel Post </h1> */}
+                <Typography variant="h1">
+                    Home
+                </Typography>
                 <Post />
             </Grid>
             <Grid item xs={12}>
-                <ChannelsList />
+              <Typography variant="body">
+                  <ListItem>
+                  {friends.map((friend)=><li>{friend.name}</li>)}
+                  </ListItem>
+              </Typography>
             </Grid>
         </Container>
     );
 };
 
-export default Home; 
