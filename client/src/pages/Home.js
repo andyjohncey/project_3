71bf4b63f1e161ca7b152a03dce15cf9a77e8964
Andy J <andrew.johncey@outlook.com>
2020-04-04 21:57:20 +1100

    remove imports and friend function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,61 +1,63 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from '../context/auth-context';
 // import { Col, Row, Container } from '';
-import Post from '../components/Post/Post';
+// import Post from '../components/Post/Post';
 import Nav from '../components/Nav/Nav';
-import ChannelsList from '../components/ChannelsList.js';
 import Sidebar from '../components/SideBar/SideBar';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import ListItem from '@material-ui/core';
-import Typography from '@material-ui/core';
+// import ListItem from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 // import Assist from '../pages/Assist';
-import SideBar from "../components/SideBar/SideBar";
-import Nav from '../components/Nav';
-import Post from '../components/Post';
-import ChannelsList from '../components/ChannelsList';
+// import SimpleComponent from './SimpleComponent' 
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function App(){
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
+
+function Home(){
 
     const {token} = useContext(AuthContext);
-  
-    const [friends, setFriends] = useState([])
-    useEffect(async ()=>{
-      const result = await fetch('/api/friends', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-      });
-      if(result.status !== 200 && result.status !== 201){
-        throw new Error('Failed!')
-      }
-      const resultJSON = await result.json();
-      setFriends(resultJSON.friends)
-    }, [])
+    console.log(token);
+    console.log("Home page oken presenetd above")
 
-
-
+    const classes = useStyles();
 
     return (
+        <div className={classes.root}>
         <Container maxWidth="xs">
             <Grid item xs={12}>
-                <Nav />
-                <SideBar />
-                {/* <h1>Channel Post </h1> */}
-                <Typography variant="h1">
+            <Typography variant="h1">
                     Home
                 </Typography>
-                <Post />
+                {/* <SimpleComponent /> */}
+                <Nav />
+                {/* <SideBar /> */}
+                <Typography variant="h3">
+                Channel Post 
+                </Typography>
+                 {/* <Post /> */}
             </Grid>
             <Grid item xs={12}>
-              <Typography variant="body">
-                  <ListItem>
-                  {friends.map((friend)=><li>{friend.name}</li>)}
-                  </ListItem>
+              <Typography>
+                  Login Worked
+                  {/* {friends.map((friend)=><li>{friend.name}</li>)} */}
+                  
               </Typography>
             </Grid>
         </Container>
+        </div>
     );
 };
 
+export default Home;
