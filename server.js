d1663930cff6a3a1016f09c3c7027b12364243e0
Andy J <andrew.johncey@outlook.com>
2020-04-04 20:53:05 +1100

    remove passport auth from server

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const express = require("express");
-// const path = require("path");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
 const routes = require("./routes");
-const morgan = require('morgan')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const dbConnection = require('./server/db') // loads our connection to the mongo database
-// const passport = require('./server/passport')
 const isAuth = require("./middleware/is-auth");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -23,6 +16,7 @@ require('dotenv').config()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(isAuth);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -35,25 +29,16 @@ app.use(routes);
 //     res.sendFile(path.join(__dirname, "./client/build/index.html"))
 // });
 
-// ==== if its production environment!
-if (process.env.NODE_ENV === 'production') {
-    const path = require('path')
-    console.log('YOU ARE IN THE PRODUCTION ENV')
-    app.use('/static', express.static(path.join(__dirname, '../build/static')))
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../build/'))
-    })
-}
+// // ==== if its production environment!
+// if (process.env.NODE_ENV === 'production') {
+//     const path = require('path')
+//     console.log('YOU ARE IN THE PRODUCTION ENV')
+//     app.use('/static', express.static(path.join(__dirname, '../build/static')))
+//     app.get('/', (req, res) => {
+//         res.sendFile(path.join(__dirname, '../build/'))
+//     })
+// }
 
-/* Express app ROUTING */
-app.use('/auth', require('./server/auth'))
-
-// ====== Error handler ====
-app.use(function (err, req, res, next) {
-    console.log('====== ERROR =======')
-    console.error(err.stack)
-    res.status(500)
-})
 
 // Connect to Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/agedcareassist");
