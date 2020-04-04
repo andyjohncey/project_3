22a24f5372dfdf7d16c89f2bdda7cf52086ffb4d
Andy J <andrew.johncey@outlook.com>
2020-04-04 22:32:19 +1100

    add authRoutes to API routes

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
+const authRoutes = require("./auth");
 const postRoutes = require("./posts");
 
 // Post routes
 router.use("/posts", postRoutes);
+router.use("/auth", authRoutes);
 
 module.exports = router;
