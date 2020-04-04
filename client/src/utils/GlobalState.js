1629a03ccc5084b8c2fb8f08d151114da57fa5cd
Andy J <andrew.johncey@outlook.com>
2020-04-04 22:06:20 +1100

    edit actions on globalstate file

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,88 +1,109 @@
 import React, { createContext, useReducer, useContext } from 'react';
-import {
-    SET_CURRENT_POST,
-    REMOVE_POST,
-    UPDATE_POST,
-    ADD_POST,
-    LOADING,
-    SET_SIDEBAR_OPEN,
-    SET_PROFILE_OPEN
-} from './actions';
+// import {
+//     SET_CURRENT_POST,
+//     REMOVE_POST,
+//     UPDATE_POST,
+//     ADD_POST,
+//     LOADING,
+//     SET_SIDEBAR_OPEN,
+//     SET_PROFILE_OPEN
+// } from './actions';
 
-const StoreContext = createContext();
+const StoreContext = createContext({
+    posts: {},
+        currentPost: {
+            _id: "",
+            title: "",
+            username: "",
+            body: ""
+            
+        },
+        loading: false,
+        isSidebarOpen: false,
+});
+console.log(StoreContext);
 const { Provider } = StoreContext;
 
-const reducer = (state, action) => {
+function reducer (state, action) {
     switch (action.type) {
-        case SET_CURRENT_POST:
-            return {
+        case "SET_CURRENT_POST":
+            return [
                 ...state,
+            {
+                _id: state,
+                
                 currentPost: action.post,
                 loading: false
-            };
-
-        case UPDATE_POST:
-            return {
+            }
+        ];
+        case "UPDATE_POST":
+            return [
                 ...state,
+                {
                 posts: [...action.posts],
                 loading: false
-            };
-
-        case ADD_POST:
-            return {
+            }
+        ];
+        case "ADD_POST":
+            return [
                 ...state,
+                {
                 posts: [action.post, ...state.posts],
                 loading: false
-            };
-
-        case REMOVE_POST:
-            return {
+            }
+        ];
+        case "REMOVE_POST":
+            return [
                 ...state,
-                posts: state.posts.filter((post) => {
-                    return post._id !== action._id;
+                {
+                posts: state.filter((_, post) => {
+                    return post._id !== action.post;
                 })
-            };
-
-        case LOADING:
-            return {
+            }
+        ];
+        case "LOADING":
+            return [
                 ...state,
+                {
                 loading: true
-            };
-
-        case SET_SIDEBAR_OPEN:
-            return {
+            }
+        ];
+        case "SET_SIDEBAR_OPEN":
+            return [
                 ...state,
+                {
                 isSidebarOpen: action.isSidebarOpen,
-            };
-
-        case SET_PROFILE_OPEN:
-            return {
+            }
+        ];
+        case "SET_PROFILE_OPEN":
+            return [
                 ...state,
+                {
                 isProfileOpen: action.isProfileOpen,
-            };
-
+            }
+        ];
         default:
             return state;
     }
 };
 
-const StoreProvider = ({ value = [], ...props }) => {
+function StoreProvider ({ value = [], ...props }) {
     const [state, dispatch] = useReducer(reducer, {
-        posts: [],
+        posts: {},
         currentPost: {
             _id: 0,
             title: "",
-            body: "",
-            username: ""
+            username: "",
+            body: ""
+            
         },
         loading: false,
-        isSidebarOpen: false,
     });
 
-    return <Provider value={[state, dispatch]} {...props} />
+    return <Provider value={[state, dispatch]} {...props} />;
 };
 
-const useStoreContext = () => {
+function useStoreContext () {
     return useContext(StoreContext);
 };
 
