d0846099a143b5c4ad78c744e7cb946d2255a1d1
Andy J <andrew.johncey@outlook.com>
2020-04-04 21:16:14 +1100

    add container to main component

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,16 +1,15 @@
 import React from "react";
 import channels from '../Channels/Channels';
 import Post from '../Post/Post';
+import container from '@material-ui/core/styles'
 
 
 function Main() {
     return (
-        <>
-
+        <Container>
             <h1>Emergency call Dial '000'</h1>
-            {/* <Channels /> */}
-            <Post />
-        </>
+           
+        </Container>
 
     )
 }
